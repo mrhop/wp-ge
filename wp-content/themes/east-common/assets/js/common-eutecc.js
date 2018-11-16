6e4601f6444c41a731ece8ab1a6728f4cef2fdbc
mrhop <233402923@qq.com>
2018-11-16 11:39:41 +0800

    wp-ge init

--- wp-content/themes/east-common/assets/js/common-eutecc.js
+++ wp-content/themes/east-common/assets/js/common-eutecc.js
@@ -25,5 +25,46 @@ $(document).ready(function () {
         return false;
     })
 
+    // click menu and the breadcrumb
+    document.querySelectorAll('#primary-menu >.menu-item >a').forEach(function (element) {
+        var subMenu = element.parentElement.querySelector('.sub-menu');
+        if (subMenu != null) {
+            element.addEventListener('click', function (e) {
+                if (subMenu.style.display === 'block') {
+                    subMenu.style.display = 'none'
+                } else {
+                    subMenu.style.display = 'block'
+                }
+                e.preventDefault()
+                e.stopPropagation()
+            })
+        }
+    })
+    document.querySelectorAll('#primary-menu > ul >.page_item >a').forEach(function (element) {
+        var subMenu = element.parentElement.querySelector('.children');
+        if (subMenu != null) {
+            element.addEventListener('click', function (e) {
+                if (subMenu.style.display === 'block') {
+                    subMenu.style.display = 'none'
+                } else {
+                    subMenu.style.display = 'block'
+                }
+                e.preventDefault()
+                e.stopPropagation()
+            })
+        }
+    })
+    var breadcrumbFirst = document.querySelector('.breadcrumbs >.container > span > a')
+    if (breadcrumbFirst) {
+        document.querySelector('.breadcrumbs >.container > span > a').addEventListener('click', function (e) {
+            e.preventDefault()
+        })
+    }
+    document.body.addEventListener('click', function () {
+        document.querySelectorAll('#primary-menu >.menu-item .sub-menu').forEach(function (element) {
+            element.style.display = 'none'
+        })
+    })
+
 });
 
